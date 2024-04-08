2546e233b935a74d21f24b2baa806ede7d8dd71f
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-04-08 20:07:46 +0700

    test 04/08/2024

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import SubjectList from './components/de7_crudMonHoc/SubjectList';
+
+const App = () => {
+  return (
+    <SubjectList/>
+  )
+}
+
+export default App;
---
+++ src/components/de7_crudMonHoc/SubjectList.jsx
@@ -0,0 +1,99 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import edit from "../../../public/image/edit.png";
+import deleteIcon from "../../../public/image/deleteIcon.png";
+import "./SubjectList.scss";
+
+const SubjectList = () => {
+    const [id, setId] = useState(0);
+    const [subject, setSubject] = useState([]);
+    const [image, setImage] = useState("");
+    const [name, setName] = useState("");
+    const [describe, setDescribe] = useState("");
+    const [editValue, setEditValue] = useState("");
+
+    const handleAdd = () => {
+        setSubject([...subject, {
+            image, name, describe
+        }]);
+        console.log(subject)
+        setImage("");
+        setName("");
+        setDescribe("");
+
+    }
+
+    const handleEditId = (id) => {
+        // setId(id);
+
+        // const updateSubject = [...subject];
+        // const itemEdit = updateSubject[id];
+
+        // setImage(itemEdit.image);
+        // setName(itemEdit.name);
+        // setDescribe(itemEdit.describe);
+    }
+
+    const handleEdit = () => {
+        // const updateSubject = [...subject];
+        // setEditValue({image, name, describe});
+        // updateSubject[id] = editValue;
+        // setSubject(updateSubject);
+
+        // setImage("");
+        // setName("");
+        // setDescribe("");
+    }
+
+    const handleDelete = (id) => {
+        // const newSubject = subject.reduce((item, index) => index !== id);
+        // setSubject(newSubject);
+    }
+    return (
+        <div className='subject'>
+            <div className='title'>
+                <p className='title-p'><b>Quản lý môn học</b></p>
+            </div>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <div className='item-form'>
+                    <label>Link ảnh môn học: </label>
+                    <br />
+                    <input value={image} type="text" onChange={(e) => setImage(e.target.value.toString())} placeholder='Nhập link ảnh' />
+                </div>
+
+                <div className='item-form'>
+                    <label>Tên môn học:</label>
+                    <br />
+                    <input value={name} type='text' onChange={(e) => setName(e.target.value)} placeholder='Nhập tên môn học' />
+                </div>
+                
+                <div className='item-form'>
+                    <label>Mô tả môn học: </label>
+                    <br />
+                    <input value={describe} type='text' onChange={(e) => setDescribe(e.target.value)} placeholder='Nhập mô tả chi tiết môn học' />
+                </div>
+                <div className='button'>
+                    <button className='button-add' type='submit' onClick={handleAdd}>Thêm môn học</button>
+                    <button className='button-edit' type='submit' onClick={handleEdit}>Cập nhật môn học</button>
+                </div>
+            </form>
+            <div className='subject-list'>
+                {subject && subject.map((item, index) => {
+                    return <div className='subject-item' key={index}>
+                            <img src={item.image} className='image-item'/>
+                            <p className='name-item'>{item.name}</p>
+                            <div className='describe-item'>{item.describe}</div>
+                            <div className='icon'>
+                                <img src={edit} onClick={handleEditId(index)} className='icon-item'/>
+                                <img src={deleteIcon} onClick={handleDelete(index)} className='icon-item'/>
+                            </div>
+                    </div>
+
+                })}
+            </div>
+
+        </div>
+    )
+}
+
+export default SubjectList;
